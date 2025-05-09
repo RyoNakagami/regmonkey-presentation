2ce20244a5b84833ab9e0b6d4307f27de3735fc2
RyoNak <nakagamiryo0901@gmail.com>
2025-05-09 13:13:10 +0900

    TEST

---
+++ _extensions/reveal_vspace/reveal_vspace.lua
@@ -0,0 +1,20 @@
+-- Inject the CSS dependency for revealjs
+local function scan_blocks(blocks)
+    if quarto.doc.isFormat("revealjs") then
+        quarto.doc.addHtmlDependency({
+            name = "reveal_vspace",
+            version = "1.0.0",
+            stylesheets = {"reveal_vspace.css"}
+        })
+    end
+    return blocks
+end
+
+function reveal_vspace(args)
+    local height = args[1] or "1em"
+
+    -- adding css dependency
+    scan_blocks(quarto.doc.blocks)
+
+    return pandoc.RawBlock("html", '<div class="reveal_vspace" style="--vspace-height: ' .. height .. ';"></div>')
+end
